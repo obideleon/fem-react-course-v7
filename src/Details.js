cd1b6b09a22bcf6ade37d885bc34f703dac82840
obideleon <robbydeleon@gmail.com>
2022-03-13 20:36:59 +0800

    10-error-boundaries

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import { useParams } from "react-router-dom";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends Component {
   state = { loading: true };
@@ -18,6 +19,8 @@ class Details extends Component {
     if (this.state.loading) {
       return <h2>Loading ...</h2>;
     }
+
+    throw new Error("Got an error");
 
     const { animal, breed, city, state, description, name, images } =
       this.state;
@@ -40,7 +43,11 @@ class Details extends Component {
 
 const WrappedDetails = () => {
   const params = useParams();
-  return <Details params={params} />;
+  return (
+    <ErrorBoundary>
+      <Details params={params} />
+    </ErrorBoundary>
+  );
 };
 
 export default WrappedDetails;
